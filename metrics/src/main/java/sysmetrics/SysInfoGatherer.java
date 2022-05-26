15d3fed2ddfcd95507d2d03db453290a210fa3c2
Belreau Cohen <nuu.chah.nulth@disroot.org>
2022-05-27 01:24:07 +0700

    feat(sysmetrics): update metrics mechanism

--- metrics/src/main/java/sysmetrics/SysInfoGatherer.java
+++ metrics/src/main/java/sysmetrics/SysInfoGatherer.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.FileStore;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,6 +15,10 @@ import oshi.hardware.GlobalMemory;
 import oshi.hardware.HardwareAbstractionLayer;
 import oshi.hardware.NetworkIF;
 
+interface UpdateCallable {
+    void update();
+}
+
 public class SysInfoGatherer {
     private List<NetworkIF> networkInterfaces;
     private CentralProcessor cpu;
@@ -23,8 +28,16 @@ public class SysInfoGatherer {
     private String[] dnsServers;
 
     private Map<String, Map<String, Double>> metrics = new HashMap<String, Map<String, Double>>();
+    private List<UpdateCallable> updaters = new ArrayList<UpdateCallable>();
 
     private SysInfoGatherer() {
+    }
+
+    public Map<String, Map<String, Double>> updateMetrics() {
+        for (UpdateCallable updater : updaters) {
+            updater.update();
+        }
+        return metrics;
     }
 
     public static Builder newBuilder() {
@@ -41,6 +54,18 @@ public class SysInfoGatherer {
         public Builder initNetworkIFs() {
             SysInfoGatherer.this.networkInterfaces = sysHAL.getNetworkIFs();
             SysInfoGatherer.this.metrics.put("network_usage", new HashMap<String, Double>());
+
+            SysInfoGatherer.this.updaters.add(() -> {
+                for (NetworkIF networkInterface : networkInterfaces) {
+                    String interfaceName = networkInterface.getDisplayName();
+                    networkInterface.updateAttributes();
+                    metrics.get("network_usage").put(interfaceName + " recv",
+                            Double.valueOf(networkInterface.getBytesRecv()));
+                    metrics.get("network_usage").put(interfaceName + " sent",
+                            Double.valueOf(networkInterface.getBytesSent()));
+                }
+            });
+
             return this;
         }
 
@@ -48,12 +73,27 @@ public class SysInfoGatherer {
             SysInfoGatherer.this.cpu = sysHAL.getProcessor();
             SysInfoGatherer.this.cpuLoadTicks = cpu.getProcessorCpuLoadTicks();
             SysInfoGatherer.this.metrics.put("cpu_usage", new HashMap<String, Double>());
+
+            SysInfoGatherer.this.updaters.add(() -> {
+                double[] recentUsage = cpu.getProcessorCpuLoadBetweenTicks(cpuLoadTicks);
+                for (int i = 0; i < recentUsage.length; i++) {
+                    metrics.get("cpu_usage").put(Integer.toString(i + 1), recentUsage[i]);
+                }
+                cpuLoadTicks = cpu.getProcessorCpuLoadTicks();
+            });
+
             return this;
         }
 
         public Builder initMemory() {
             SysInfoGatherer.this.ram = sysHAL.getMemory();
             SysInfoGatherer.this.metrics.put("memory_usage", new HashMap<String, Double>());
+
+            SysInfoGatherer.this.updaters.add(() -> {
+                double usedToTotal = 1 - (ram.getAvailable() / (double) ram.getTotal());
+                metrics.get("memory_usage").put("used/total", usedToTotal);
+            });
+
             return this;
         }
 
@@ -61,6 +101,17 @@ public class SysInfoGatherer {
                 throws IOException {
             SysInfoGatherer.this.disk = Files.getFileStore(Paths.get("./"));
             SysInfoGatherer.this.metrics.put("disk_usage", new HashMap<String, Double>());
+
+            SysInfoGatherer.this.updaters.add(() -> {
+                double usedToTotal;
+                try {
+                    usedToTotal = 1 - (disk.getUsableSpace() / (double) disk.getTotalSpace());
+                } catch (IOException e) {
+                    usedToTotal = 1.0;
+                }
+                metrics.get("disk_usage").put("used/total", usedToTotal);
+            });
+
             return this;
         }
 
