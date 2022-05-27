1e033f3d39a9166f42bef13c7c05f8464da099c7
Belreau Cohen <nuu.chah.nulth@disroot.org>
2022-05-27 12:42:51 +0700

    feat(sysmetrics): ConsoleLog logging metrics every 3 seconds

--- metrics/src/main/java/sysmetrics/ConsoleLog.java
+++ metrics/src/main/java/sysmetrics/ConsoleLog.java
@@ -1,8 +1,41 @@
 package sysmetrics;
 
-public class ConsoleLog
-{
+import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import com.fasterxml.jackson.core.JsonGenerator.Feature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+public class ConsoleLog {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            SysInfoGatherer sysInfo = SysInfoGatherer.newBuilder()
+                    .initNetworkIFs()
+                    .initCPU()
+                    .initDisk()
+                    .initMemory()
+                    .initDNS("8.8.8.8", "1.1.1.1", "77.88.8.8")
+                    .build();
+
+            var mapper = new ObjectMapper(new YAMLFactory().disable(Feature.AUTO_CLOSE_TARGET));
+
+            ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+
+            executor.scheduleAtFixedRate(() -> {
+                var metrics = sysInfo.updateMetrics();
+                try {
+                    mapper.writeValue(System.out, metrics);
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
+            }, 0, 3, TimeUnit.SECONDS);
+
+        } catch (IOException e) {
+            System.out.println("Возникла ошибка при инициализации метрик!");
+            System.err.println(e.getMessage());
+        }
     }
 }
