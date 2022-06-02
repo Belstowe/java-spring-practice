05bc0fa77f0a3adb49706ed47327660f08e24b1e
Belreau Cohen <nuu.chah.nulth@disroot.org>
2022-06-03 01:18:32 +0700

    feat(sysmetrics): getGroups and getByTimeRange methods; add test

--- metrics/src/main/java/com/sysmetrics/main/SysInfoCQL.java
+++ metrics/src/main/java/com/sysmetrics/main/SysInfoCQL.java
@@ -1,13 +1,17 @@
 package com.sysmetrics.main;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 
 import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
 
 public class SysInfoCQL
@@ -15,9 +19,11 @@ public class SysInfoCQL
 
     private Cluster cluster;
     private Session session;
+    private String namespace;
     private Collection<DBException> exceptions = new ArrayList<DBException>();
 
     public SysInfoCQL(String contactPoint, String namespace, String username, String password) {
+        this.namespace = namespace;
         var clusterBuilder = Cluster.builder()
                 .withoutJMXReporting()
                 .addContactPoint(contactPoint);
@@ -77,4 +83,64 @@ public class SysInfoCQL
         return exceptions;
     }
 
+    @Override
+    public String[] getGroups() {
+        return cluster.getMetadata()
+                .getKeyspace(namespace)
+                .getTables()
+                .stream()
+                .map((metadata) -> {
+                    return metadata.getName();
+                })
+                .toArray(String[]::new);
+    }
+
+    private Map<String, Map<String, Double>> requestTimeRange(
+            LocalDate date, LocalTime from, LocalTime to, String table) {
+        String selectStatement = "SELECT * FROM " + table + " WHERE infodate = '" + date + "'";
+        if (from == null) {
+            selectStatement += " AND infotime >= '" + from + "'";
+        }
+        if (to == null) {
+            selectStatement += " AND infotime <= '" + to + "'";
+        }
+        selectStatement += ";";
+        ResultSet rs = session.execute(selectStatement);
+        var columns = rs.getColumnDefinitions();
+        var dateString = date.toString();
+
+        var result = new HashMap<String, Map<String, Double>>();
+        for (Row row : rs) {
+            var metaTime = dateString + "T" + LocalTime.ofNanoOfDay(row.getTime("infotime")).toString();
+            var metrics = new HashMap<String, Double>();
+            for (int i = 2; i < columns.size(); i++) {
+                metrics.put(columns.getName(i), row.getDouble(i));
+            }
+            result.put(metaTime, metrics);
+        }
+        return result;
+    }
+
+    @Override
+    public Map<String, Map<String, Map<String, Double>>> selectTimeRange(
+            LocalDateTime from, LocalDateTime to, Collection<String> groups) {
+        var fromDate = from.toLocalDate();
+        var toDate = to.toLocalDate();
+        var result = new HashMap<String, Map<String, Map<String, Double>>>();
+
+        for (var date = fromDate; date.isBefore(toDate) || date.isEqual(toDate); date = date.plusDays(1)) {
+            var fromTime = (date.isEqual(fromDate) ? from.toLocalTime() : null);
+            var toTime = (date.isEqual(toDate) ? to.toLocalTime() : null);
+            for (var group : groups) {
+                result.put(group, requestTimeRange(date, fromTime, toTime, group));
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public void destroy() {
+        session.execute("DROP KEYSPACE " + namespace + ";");
+    }
 }
